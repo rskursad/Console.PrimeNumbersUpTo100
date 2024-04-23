78ef8bd1136b7a6e98829048b5090d76d8671869
Kürşad's PC <Krad's PC@DESKTOP-URFB7VL>
2024-04-23 17:23:53 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _100ekadarOlanAsalSayilar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            for (int i = 2; i < 100; i++)
+            {
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+                    if (count == 2)
+                    {
+                        break;
+                    }
+                }            
+                if(count==2)
+                {
+                    count = 0;
+
+                }
+                else
+                {
+                    count = 0;
+                    Console.Write(i+" ");
+                }
+            }
+        }
+    }
+}
